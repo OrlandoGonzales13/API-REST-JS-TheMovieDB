54922cb5c9b8f7a3a8ef93ac1642b4e5d72b3f58
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-10-19 16:38:00 -0500

    [GET] PELICULAS EN TENDENDIA PARA EL HOME - PREVIEW

--- src/main.js
+++ src/main.js
@@ -1,2 +1,28 @@
+//GET -  PELICULAS EN TENDENDIA PARA EL HOME - PREVIEW
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY)
+    const data = await res.json();
 
-console.log("APIU URL" + API_KEY)
+    const movies = data.results;
+    //console.log({ data, movies }); //revisar datos que traemos
+
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute(
+            'src',
+            'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+        );
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+}
+
+getTrendingMoviesPreview();
