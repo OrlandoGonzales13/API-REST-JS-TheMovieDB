69fc4ebdea0c3abff17361271ab08f6448785968
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-10-22 19:29:32 -0500

    [BUG] CARGA DUPLICADA DE DATOS

--- src/main.js
+++ src/main.js
@@ -16,8 +16,9 @@ async function getTrendingMoviesPreview() {
     const movies = data.results;
     //console.log({ data, movies }); //revisar datos que traemos
 
+    trendingMoviesPreviewList.innerHTML = " ";
+
     movies.forEach(movie => {
-        const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
 
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
@@ -40,8 +41,9 @@ async function getCategoriesPreview() {
     const { data } = await api('genre/movie/list');
     const categories = data.genres;
 
+    categoriesPreviewList.innerHTML = "";
+
     categories.forEach(category => {
-        const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
 
         const categoryContainer = document.createElement('div');
         categoryContainer.classList.add('category-container');
